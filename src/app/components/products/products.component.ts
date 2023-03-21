27e744a8ffe4576e935cfce740e6d544f84049aa
JhordyMora <jhordy.mora@web.de>
2023-03-21 02:11:38 +0100

    added styles to the products component

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -27,5 +27,25 @@ export class ProductsComponent {
     imageUrl: 'https://i.redd.it/jeuusd992wd41.jpg',
     price: Math.random() * 100,
   };
-  products: product[] = [this.product1, this.product2, this.product3];
+  product4: product = {
+    id: '4',
+    name: 'product4',
+    imageUrl:
+      'https://cdn.pixabay.com/photo/2017/11/13/07/14/cats-eyes-2944820_960_720.jpg',
+    price: Math.random() * 100,
+  };
+  product5: product = {
+    id: '5',
+    name: 'product5',
+    imageUrl:
+      'https://static.giga.de/wp-content/uploads/2021/09/random-GettyImages-531316319-rcm850x478u.jpg',
+    price: Math.random() * 100,
+  };
+  product6: product = {
+    id: '6',
+    name: 'product6',
+    imageUrl: 'https://i.redd.it/jeuusd992wd41.jpg',
+    price: Math.random() * 100,
+  };
+  products: product[] = [this.product1, this.product2, this.product3,this.product4, this.product5, this.product6];
 }
