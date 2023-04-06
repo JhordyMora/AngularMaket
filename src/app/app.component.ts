a453fa2e0858575b125be6129f8c48fed3e5d849
JhordyMora <jhordy.mora@web.de>
2023-04-06 06:43:43 +0200

    created basic  logic for user creation auth and token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,11 @@ export class AppComponent {
   imageSrc = 'Agrege la url de la imagen que quiera mostrar'; //"https://www.w3schools.com/howto/img_avatar.png";
   showImage = true;
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ){}
+
   onLoaded(img: string) {
     console.log('Log padre', img);
   }
@@ -18,4 +25,22 @@ export class AppComponent {
   toggleImage() {
     this.showImage = !this.showImage;
   }
+  
+  createUser(){
+    this.usersService.createUser({
+      "name":"donPrueba",
+      "email": "prueba@mail.com", 
+      "password": "prueba"
+    }).subscribe({
+      next: (dataRta)=>{console.log(dataRta)},
+    });
+  }
+
+  login(){
+    this.authService.login(
+      "prueba@mail.com", 
+      "prueba"
+    ).subscribe({
+      next: (dataRta)=>{console.log(dataRta.access_token)},
+    });}
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreateUserDTO, User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  
+  private apiUrl ='https://young-sands-07814.herokuapp.com/api/users'; 
+  
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  createUser(createUserDto: CreateUserDTO){
+    return this.http.post<User>(this.apiUrl, createUserDto);
+  }
+  
+  getAllUsers(){
+    return this.http.get<User[]>(this.apiUrl);
+  }
 }
